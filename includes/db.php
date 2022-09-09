e5269fd19ae3176ca6d1327c253cdd527baf82a9
faisalahammed044274 <setu2014@gmail.com>
2022-09-10 04:59:46 +0600

    Category views and Posts Table Created

--- includes/db.php
+++ includes/db.php
@@ -11,8 +11,8 @@ foreach ($db as $key => $value) {
 }
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if ($connection) {
-    echo "DB Connected";
-} else {
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($connection) {
+//     echo "DB Connected";
+// } else{
+//     die("Connection_failed :" . $connection->connect_error);
+// }
