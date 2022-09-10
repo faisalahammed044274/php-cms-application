498e6c13d47573a2d42abfa48335cb947c27333f
faisalahammed044274 <setu2014@gmail.com>
2022-09-11 05:57:04 +0600

    Nav link in admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
 <div id="wrapper">
 
     <!-- Navigation -->
-<?php include "includes/navigation.php" ?>
+    <?php include "includes/navigation.php" ?>
 
     <div id="page-wrapper">
 
@@ -13,15 +13,15 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                        Blank Page
-                        <small>Subheading</small>
+                        Welcome Admin
+                        <small>Start your work and enjoy !</small>
                     </h1>
                     <ol class="breadcrumb">
                         <li>
                             <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
                         </li>
                         <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
+                            <i class="fa fa-file"></i> Author Page
                         </li>
                     </ol>
                 </div>
