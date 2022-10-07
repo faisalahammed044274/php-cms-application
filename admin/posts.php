c8efeb4da70436fcc4d60e4c01a03e62edb10739
faisalahammed044274 <setu2014@gmail.com>
2022-10-07 23:50:56 +0600

    post table design fixing

---
+++ admin/posts.php
@@ -0,0 +1,62 @@
+<?php include "includes/admin_header.php" ?>
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php" ?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <!-- oooooooooooooooooooooooooooooooooooooooooooo -->
+                    <h1 class="page-header">
+                        Hey, Admin ! Welcome to Posts Page
+
+                        <small>See your posts here</small>
+                    </h1>
+
+                    <table class="table" style="padding: 5px 8px;">
+                        <thead>
+                            <th>Id</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                        </thead>
+                        <tbody>
+                            <td>10</td>
+                            <td>me</td>
+                            <td>bootstrap framework</td>
+                            <td>bootstrap</td>
+                            <td>Status</td>
+                            <td>IMgae</td>
+                            <td>tags</td>
+                            <td>COmments</td>
+                            <td>30.8.22</td>
+                        </tbody>
+                    </table>
+
+
+
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+
+<?php include "includes/admin_footer.php" ?>
