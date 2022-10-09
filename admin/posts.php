e861a862ebc2d45ac56f83415db57d61510ae1bb
faisalahammed044274 <setu2014@gmail.com>
2022-10-09 19:34:32 +0600

    posts

--- admin/posts.php
+++ admin/posts.php
@@ -19,49 +19,32 @@
                         <small>See your posts here</small>
                     </h1>
 
-                    <table class="table" style="padding: 5px 8px;">
-                        <thead>
-                            <th>Id</th>
-                            <th>Author</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Image</th>
-                            <th>Tags</th>
-                            <th>Comments</th>
-                            <th>Date</th>
-                        </thead>
-                        <tbody>
 
-                            <?php
+                    <?php
 
-                            $query = "SELECT * FROM posts";
-                            $select_posts = mysqli_query($connection, $query);
+                        if(isset($_GET['source'])){
+                            $source = $_GET['source'];
+                        } else{
+                            $source = '';
+                        }
+                        switch($source){
+                            case '34';
+                            echo "Nice 34";
+                            break;
+                            case '100';
+                            echo "Nice 100";
+                            break;
+                            case '200';
+                            echo "Nice 200";
+                            break;
 
-                            while ($row = mysqli_fetch_assoc($select_posts)) {
-                                $post_id = $row['post_id'];
-                                $post_author = $row['post_author'];
-                                $post_title = $row['post_title'];
-                                $post_category_id = $row['post_category_id'];
-                                $post_stats = $row['post_stats'];
-                                $post_image = $row['post_image'];
-                                $post_tags = $row['post_tags'];
-                                $post_comment_count = $row['post_comment_count'];
-                                $post_date = $row['post_date'];
+                            default:
+                            include"includes/view_all_posts.php";
+                            break;
 
-                                echo "<tr>";
-                                echo "<td>$post_id</td>";
-                                echo "<td>$post_author</td>";
-                                echo "<td>$post_title</td>";
-                                echo "<td>$post_category_id</td>";
-                                echo "<td>$post_stats</td>";
-                                echo "<td><img class='img-responsive' src='../images/$post_image' alt='image'></td>";
-                                echo "<td>$post_tags</td>";
-                                echo "<td>$post_comment_count</td>";
-                                echo "<td>$post_date</td>";
-                                echo "</tr>";
-                            }
-                            ?>
+                        }
+?>
+
                         </tbody>
                     </table>
 
